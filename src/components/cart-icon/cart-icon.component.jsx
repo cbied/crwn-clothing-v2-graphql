5542439e0f01ace84792fad06bb8dd0fbaf60a93
Biediger <cabiediger@gmail.com>
2023-12-21 02:56:41 -0500

    add graphQL, add graphQL to categories context

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,7 +9,9 @@ import { CartIconContainer, ItemCount } from './cart-icon.styles';
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const toggleIsCartOpen = () => {
+    setIsCartOpen(!isCartOpen);
+  }
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,43 @@
 import { createContext, useState, useEffect } from 'react';
-
+import { gql, useQuery } from '@apollo/client';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+    id 
+    title
+    items {
+      id
+      name
+      price
+      imageUrl
+    }
+  }
+  }
+`
+
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  const { loading, data } = useQuery(COLLECTIONS)
+  const [categoriesMap, setCollectionsMap] = useState({});
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+    if(data) {
+      const { collections } = data;
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection
+        acc[title.toLowerCase()] = items
+        return acc
+    }, {})
+      setCollectionsMap(collectionsMap)
+    }
+  }, [data])
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
